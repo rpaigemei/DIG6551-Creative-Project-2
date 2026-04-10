d0a2e16abcb8c981230c7f67f4b5cf651c2f9f91
rpaigemei <paigereeves@ufl.edu>
2026-04-10 01:14:09 -0400

    letter puzzle complete

--- src/pages/LetterPuzzle.jsx
+++ src/pages/LetterPuzzle.jsx
@@ -4,6 +4,7 @@ import { motion } from "motion/react"
 import { containerVariants, lineVariants } from "../components/text-fade-in"
 import LetterNote from "../assets/notes/letter-note.png"
 import PopupNote from "../assets/notes/popup-note.png"
+import InputPaper from "../assets/input-paper.png"
 
 export function LetterPuzzle({ onSolved }) {
   const [clues, setClues] = useState([
@@ -11,12 +12,12 @@ export function LetterPuzzle({ onSolved }) {
     {id: 1, clue: "When the lion lyrically sleeps", answer: "tonight", solved: false},
     {id: 2, clue: "Comedy like How to Lose a Guy in 10 Days or 10 Things I Hate About You", answer: "romantic", solved: false},
     {id: 3, clue: "Haunted estate", answer: "mansion", solved: false},
-    {id: 4, clue: "“What goes _____ must come down”", answer: "up", solved: false},
+    {id: 4, clue: "“What goes __ must come down”", answer: "up", solved: false},
     {id: 5, clue: "One of three that make a right", answer: "left", solved: false},
     {id: 6, clue: "Dewey's domain", answer: "library", solved: false},
     {id: 7, clue: "10/10", answer: "perfect", solved: false},
     {id: 8, clue: "Like a hyena at a comedy show", answer: "laughing", solved: false},
-    {id: 9, clue: "Please --> _____ <-- and see", answer: "wait", solved: false},
+    {id: 9, clue: "Please → ____ ← and see", answer: "wait", solved: false},
   ]);
   const [guess, setGuess] = useState("");
 
@@ -25,7 +26,7 @@ export function LetterPuzzle({ onSolved }) {
   }
 
   useEffect(() => {
-    const answer = clues.find(c => c.answer === guess && !c.solved);
+    const answer = clues.find(c => c.answer === guess.toLowerCase() && !c.solved);
 
     if (answer) {
       markSolved(answer);
@@ -36,10 +37,16 @@ export function LetterPuzzle({ onSolved }) {
     const clue = clues.find(c => c.id === id);
 
     if (!clue.solved) {
-      return <span className="clue">[{clue.clue}]</span>
+      return <span className="clue">({clue.clue})</span>
     }
     
-    return <span className="answer">{clue.answer}</span>
+    return (
+      <motion.span variants={containerVariants} initial="hidden" animate="visible">
+        <motion.span key={clue.id} variants={lineVariants}>
+          {clue.answer}
+        </motion.span>
+      </motion.span>
+    )
   }
 
   const markSolved = (item) => {
@@ -101,23 +108,18 @@ export function LetterPuzzle({ onSolved }) {
       {/* connect strings puzzle */}
       <div className="puzzle">
         <div className="letter">
-          <div className="br">
+          <div className="p">
             Hello my {checkSolved(0)},
           </div>
-          <div className="br">
-            I can’t stop thinking about {checkSolved(1)}... I can hardly wait. I’ve finally found a place 
-            that’s completely ours, a {checkSolved(2)} little spot where no one else will bother us, 
-            Just for the two of us, finally. When you get to the {checkSolved(3)}, go {checkSolved(4)}stairs, 
-            down the {checkSolved(5)} hallway, and through the {checkSolved(6)}. There’s a small room tucked  
-            away that most people would never notice. I found it by accident and kept thinking how {checkSolved(7)} 
-            it would be for us. I keep imagining us there, {checkSolved(8)}, talking, just having a world of our own.
+          <div className="p">
+            I can’t stop thinking about {checkSolved(1)}... I can hardly wait. I’ve finally found a place that’s completely ours, a {checkSolved(2)} little spot where no one else will bother us. Just for the two of us, finally. When you get to the {checkSolved(3)}, go {checkSolved(4)}stairs, down the {checkSolved(5)} hallway, and through the {checkSolved(6)}. There’s a small room tucked away that most people would never notice. I found it by accident and kept thinking how {checkSolved(7)} it would be for us. I keep imagining us there, {checkSolved(8)}, talking, just having a world of our own.
           </div>
           <div>
             I’ll be {checkSolved(9)}ing for you.
           </div>
         </div>
 
-        <input className="input" type="text" value={guess} onChange={handleChange} disabled={solved ? true : false} />
+        <input className={`input ${solved && 'disabled'}`} type="text" value={guess} onChange={handleChange} disabled={solved ? true : false} placeholder="Type any answer..." />
       </div>
     </div>
   )
